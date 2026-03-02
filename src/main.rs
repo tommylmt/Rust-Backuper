6203845e8525279f363cad1d6bc64518323dacae
Tommy-dotcom <totolamothe65@gmail.com>
2026-03-02 13:27:05 +0100

    feat: add methodology before starting

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    /* TODO:
+        - Preflight check: 
+            do we have a configuration file in /etc/rust-backuper/backuper.conf
+            if not, create one and ask user to configure it
+        - Savers:
+            do a backup for each category of the backuper.conf files
+        - Bundlers:
+            pack elements with "archive = true" in config file in "zip"
+        - Transporters:
+            sync elements to the configured endpoint (Google Drive)
+        - Cleaners:
+            remove local backups once sync is OK
+    */      
+    println!("Hello, World!");
 }
